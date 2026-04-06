1645019c0dc26c10d4e5158eb9e96b8f63c46284
anandcoder24 <w.anandmarketing@gmail.com>
2026-04-06 15:00:04 +0000

    adding project files

---
+++ fileServer.js
@@ -0,0 +1,89 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+
+
+app.get("/files",(req,res)=>{
+    fs.readdir("./files",(err,data)=>{
+      console.log(data);
+
+      if(err){
+        console.log("error ",err.message);
+      }
+
+
+      res.status(200).json({
+          "content" : data
+    })
+    })
+
+    
+})
+
+app.get("/files/:filename",(req,res)=>{
+     let filename = req.params.filename;
+     let filePath = path.join(__dirname,"files",filename);
+    
+    // fs.readFile(filePath,"utf-8",(err,data)=>{
+    //   console.log(data);
+      res.sendFile(filePath,(err)=>{
+          if(err){
+            console.log("error: ",err.message);
+          }
+      });
+
+    //   if (err) {
+    //   console.log("ERROR:", err.message); // 🔥 important debug
+
+    //   return res.status(404).json({
+    //     message: "File not found",
+    //   });
+    // }
+
+    //    res.status(200).json({
+    //       "content" : data
+    // })
+    // }) 
+})
+
+app.listen(PORT,()=>{
+  console.log(`server listening ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
